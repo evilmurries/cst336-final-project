0f8fdd36271a61811f2ec72793dca33bec870756
xpy <cpiwarski01@gmail.com>
2019-08-15 20:07:44 -0700

    paid db

--- app.js
+++ app.js
@@ -565,6 +565,7 @@ app.get("/getPetInfo", function (req, res) {
   
 });
 
+/*
 // Create a connection to the database server
 function createDBConnection() {
     var conn = mysql.createConnection({
@@ -574,28 +575,28 @@ function createDBConnection() {
     database:"groupProject"//fill in the database name after it has been created in phpmyadmin
   });
     return conn;
-}
+} */
 
 // Create a multi query connection to the database server
 function createDBConnectionMultiple() {
     var conn = mysql.createPool({
         connectionLimit: 2,
         host: "us-cdbr-iron-east-02.cleardb.net",
-        user: "b966e7405b082e",
-        password: "e739afd6",
-        database: "heroku_d27a5db666d1cf0",
+        user: "b84ac7bae81020",
+        password: "94edbdb4",
+        database: "heroku_004b60bb60d84c8",
         multipleStatements: true
     });
     return conn;
 }
 
-/*
+
 //old createDBConnection insides
 var conn = mysql.createPool({
         connectionLimit: 7,
         host: "us-cdbr-iron-east-02.cleardb.net",
-        user: "b966e7405b082e",
-        password: "e739afd6",
-        database: "heroku_d27a5db666d1cf0"
+        user: "b84ac7bae81020",
+        password: "94edbdb4",
+        database: "heroku_004b60bb60d84c8"
     });
-*/
+
